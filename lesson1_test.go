e79c641cd63d191edef7bcc76d588d1bd2765582
GraceYongqi <fionayongqi@163.com>
2021-04-11 21:32:02 +0800

    优化目录结构

--- lesson1_test.go
+++ lesson1_test.go
@@ -1,14 +1,15 @@
-package main
+package goLearning
 
 import (
 	"fmt"
 	"goLearning/utils"
+	"testing"
 )
 
 
-
-func main() {
+func TestCalulateMax(t *testing.T) {
 	target := []int {199,23,41,67,34,90,1}
-
 	fmt.Println(utils.CalculateMax(target))
 }
+
+
